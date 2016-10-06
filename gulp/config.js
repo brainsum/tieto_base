a770c1ed66185638f87c402912ed1c2826a63090
Gergely Pap <gpap@brainsum.com>
2016-10-06 16:16:57 +0200

    Added config.local.js loading

--- gulp/config.js
+++ gulp/config.js
@@ -1,4 +1,7 @@
-export default {
+import { statSync } from 'fs';
+import extend from 'extend';
+
+let config = {
   project: './',
 
   styles: {
@@ -14,8 +17,19 @@ export default {
   },
 
   browserSync: {
-    proxy: 'http://tietotheme.local',
-    stream: true,
     notify: false
   }
 }
+
+// If a local config file exists, merge it into this config.
+// This is good for overriding some options which should
+// not be commited to the repo, e.g. browserSync proxy.
+try {
+  let localConfigFile = 'config.local.js';
+  statSync(`${__dirname}/${localConfigFile}`);
+  extend(true, config, require('./' + localConfigFile).default)
+} catch (err) {
+  console.warn('asdfasdf');
+}
+
+export default config
