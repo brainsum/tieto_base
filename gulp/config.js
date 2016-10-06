692fc6aff56a57e7c3e11593b4c350f868050180
Gergely Pap <gpap@brainsum.com>
2016-10-06 18:45:47 +0200

    Gulp: fixed styles task and tweaked config.js

--- gulp/config.js
+++ gulp/config.js
@@ -1,8 +1,8 @@
 import { statSync } from 'fs';
 import extend from 'extend';
+import util from 'gulp-util';
 
 let config = {
-  project: './',
 
   styles: {
     watch: './assets/scss/**/*.{sass,scss}',
@@ -18,18 +18,35 @@ let config = {
 
   browserSync: {
     notify: false
+  },
+
+  sass: {
+    outputStyle: 'expanded',
+    includePaths: [
+
+    ]
+  },
+
+  autoprefixer: {
+    browsers: ['last 2 versions']
+  },
+
+  cssnano: {
+    discardComments: {
+      removeAll: true
+    }
   }
 }
 
 // If a local config file exists, merge it into this config.
 // This is good for overriding some options which should
 // not be commited to the repo, e.g. browserSync proxy.
+const localConfigFile = 'config.local.js';
 try {
-  let localConfigFile = 'config.local.js';
   statSync(`${__dirname}/${localConfigFile}`);
   extend(true, config, require('./' + localConfigFile).default)
 } catch (err) {
-  console.warn('asdfasdf');
+  util.log(util.colors.yellow(`No ${localConfigFile} file found! If you want to override the configuration, create one.`));
 }
 
 export default config
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,34 @@
 import gulp from 'gulp';
 import config from '../config';
 import sass from 'gulp-sass';
+import cssnano from 'gulp-cssnano';
 import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
 import browserSync from 'browser-sync';
+import util from 'gulp-util';
 
 gulp.task('styles', () => {
+
   gulp
+    // Get these style assets...
     .src(config.styles.entry)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(sourcemaps.write('.'))
+
+    // If not running with --production, init sourcemaps.
+    .pipe(util.env.production ? util.noop() : sourcemaps.init())
+
+    // Convert the assets to plain CSS.
+    .pipe(sass(config.sass)).on('error', sass.logError)
+
+    // Add vendor prefixes for older browsers.
+    .pipe(autoprefixer(config.autoprefixer))
+
+    // If not running with --production, create sourcemaps,
+    // otherwise compress the CSS.
+    .pipe(util.env.production ? cssnano(config.cssnano) : sourcemaps.write('.'))
+
+    // Save the final output.
     .pipe(gulp.dest(config.styles.dest))
+
+    // Notify the browser that compile is finished.
     .pipe(browserSync.stream());
 });
