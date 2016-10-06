bee978a7014e811121f921be4e323299eaff3b5c
Gergely Pap <gpap@brainsum.com>
2016-10-06 13:49:21 +0200

    Fixed semicolons

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -3,11 +3,13 @@ import babel from 'gulp-babel';
 import config from '../config';
 import browserSync from 'browser-sync';
 
-gulp.task('scripts', () =>
-  gulp.src(config.scripts.entry)
+gulp.task('scripts', () => {
+  gulp
+    .src(config.scripts.entry)
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest(config.scripts.dist))
     .pipe(browserSync.reload(config.browserSync));
-)
+});
+
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,8 +4,9 @@ import sass from 'gulp-sass';
 import browserSync from 'browser-sync';
 
 gulp.task('styles', () => {
-  return gulp.src(config.styles.entry)
+  gulp
+    .src(config.styles.entry)
     .pipe(sass())
     .pipe(gulp.dest(config.styles.dest))
     .pipe(browserSync.reload(config.browserSync));
-})
+});
