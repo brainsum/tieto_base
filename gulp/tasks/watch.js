ed06c4f7df6c51bf4c579ba5fb0c7bebd805175b
Gergely Pap <gpap@brainsum.com>
2016-10-17 16:38:51 +0200

    Gulp: Don't build before watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 import config from '../init';
 import gulp from 'gulp';
 
-gulp.task('watch', ['build', 'reload'], () => {
+gulp.task('watch', ['reload'], () => {
   gulp.watch(config.styles.watch, ['styles']);
   gulp.watch(config.scripts.entry, ['scripts']);
 });
