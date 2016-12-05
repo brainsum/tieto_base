161427783b727b349b974cc03e276b3c88bad2b1
Gergely Pap <gpap@brainsum.com>
2016-12-05 17:54:05 +0100

    Removed dummy javascript loading on every page

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -3,14 +3,12 @@
  * Global JavaScript, which loads on every page.
  */
 
-// @todo fix imports
 (function ($, Drupal) {
   'use strict';
 
   Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
-      console.log('sup');
-      $('.asdfasdf').hide();
+
     }
   };
 
