1d6a49e97134cf569965269ce89452b1abdcfb9d
Gergely Pap <gpap@brainsum.com>
2017-01-04 20:32:17 +0100

    Override Bootstrap::isFront() because of core issue

--- src/TietoBase.php
+++ src/TietoBase.php
@@ -33,4 +33,22 @@ class TietoBase extends Bootstrap {
     return (bool) \Drupal::service('router.admin_context')->isAdminRoute($route);
   }
 
+  /**
+   * Determine if the current route is the frontpage.
+   *
+   * This overrides Bootstrap's version because there is no default
+   * "cache_context.url.path.is_front" cache context in Drupal core.
+   * @see https://www.drupal.org/node/2829588
+   *
+   * @deprecated Remove this function after 8.3.x is released.
+   *
+   * @return boolean
+   */
+  public static function isFront() {
+    if (parent::hasIsFrontCacheContext()) {
+      return parent::isFront();
+    }
+    return \Drupal::service('path.matcher')->isFrontPage();
+  }
+
 }
