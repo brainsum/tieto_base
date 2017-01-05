605e57c975eb9ba0cb576ccb5c51689cafdba75d
Gergely Pap <gpap@brainsum.com>
2017-01-05 19:19:16 +0100

    Added ThemeSuggestions for placeholder

---
+++ src/Plugin/Alter/ThemeSuggestions.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\tieto_base\Plugin\Alter;
+
+use Drupal\bootstrap\Plugin\Alter\ThemeSuggestions as BootstrapThemeSuggestions;
+use Drupal\bootstrap\Annotation\BootstrapAlter;
+use Drupal\bootstrap\Plugin\PluginBase;
+use Drupal\bootstrap\Utility\Unicode;
+use Drupal\bootstrap\Utility\Variables;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Implements hook_theme_suggestions_alter().
+ *
+ * @ingroup plugins_alter
+ *
+ * @BootstrapAlter("theme_suggestions")
+ */
+class ThemeSuggestions extends BootstrapThemeSuggestions {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(&$suggestions, &$context1 = NULL, &$hook = NULL) {
+    parent::alter($suggestions, $context1, $hook);
+    // Placeholder. Write theme suggestion changes here.
+    // $variables = Variables::create($context1);
+  }
+
+}
