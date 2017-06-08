cb62c892ce37be168f65b73c32fa89079073214b
Krisztian Pinter <kpinter@brainsum.com>
2017-06-08 18:30:28 +0200

    QAS-80

--- js/tabs-toggle.js
+++ js/tabs-toggle.js
@@ -8,7 +8,13 @@
 
   Drupal.behaviors.tietoBaseTabsToggle = {
     attach: function (context, settings) {
+      var dropdown_split_button = $('.btn-group .js-form-submit');
 
+      dropdown_split_button.on('click', function(){
+        var dropdown_split_list = $(this).siblings('.dropdown-menu');
+        var dropdown_split_button_url = dropdown_split_list.children('#dropdown-item').attr('href');
+        window.location.href = dropdown_split_button_url;
+      });
     }
   };
 
